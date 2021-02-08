13b9c4331bd9a5d1543e34c256c8728da03776cf
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-02-08 12:40:32 +0530

    Added Name component and blinking cursor.

---
+++ src/components/Name.jsx
@@ -0,0 +1,11 @@
+import './Name.css'
+
+function Name () {
+	return (
+		<div className="root-name-div">
+			<h1>project title<span class="blinking-cursor">|</span></h1>
+		</div>
+	);
+};
+
+export default Name;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+import Name from '../components/Name'
 import './Home.css'
 
 class Home extends React.Component {
     render() {
         return (
             <div className="root-home-div">
-                <h1>ayooo</h1>
+                <Name />
             </div>
         )
     }
