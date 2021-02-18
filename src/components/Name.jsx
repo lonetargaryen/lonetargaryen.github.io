b700fe6431e1dc0524ad638f8a514b2672b43be8
lonetargaryen5 <swarnim.sonawane@gmail.com>
2021-02-18 17:37:50 +0530

    Add typing effect to projects page title.

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -3,36 +3,6 @@ import React from 'react'
 import './Name.css'
 
 class Name extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		i: 0,
-	// 		initialtxt: "",
-	// 		txt: "Swarnim Sonawane",
-	// 		speed: 50
-	// 	}
-	// }
-
-	// typeWriter = () => {
-	// 	if (this.state.i < this.state.txt.length) {
-	// 		// document.getElementById("demo").innerHTML += this.state.txt.charAt(this.state.i);
-	// 		this.tick();
-	// 		setTimeout(this.typeWriter, this.state.speed);
-	// 	}
-	// }
-
-	// tick = () => {
-	// 	this.setState({
-	// 		i: this.state.i + 1
-	// 	});
-	//   }
-
-	// componentDidMount() {
-	// 	this.typeWriter();
-	// }
-
-	
-
 	render() {
 		return (
 			<div className="root-name-div">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,11 +12,34 @@ class Projects extends React.Component {
         };
     }
 
+    typeWriter = () => {
+		if (this.state.i < this.state.finalTitle.length) {
+			this.tick();
+			setTimeout(this.typeWriter, 200);
+		}
+	}
+
+	tick = () => {
+		this.setState({
+            pageTitle: this.state.pageTitle + this.state.finalTitle.charAt(this.state.i),
+			i: this.state.i + 1
+		});
+	}
+
+    // randomInteger(min, max) {
+    //     // Get a random int between min and max
+    //     return Math.floor(Math.random() * (max - min + 1)) + min;
+    // }
+
+	componentDidMount() {
+		this.typeWriter();
+	}
+
     render() {
         return (
             <div className="root-projects-div">
                 <div className="projects-heading">
-                    <h1>{this.state.finalTitle}</h1>
+                    <h1>{this.state.pageTitle}<span class="blinking-cursor">|</span></h1>
                 </div>
                 <div className="projects-content">
 
