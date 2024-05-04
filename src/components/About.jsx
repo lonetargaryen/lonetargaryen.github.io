82fcf3695602fd93674ce19950de5ba9b60c4692
lonetargaryen5 <swarnim.sonawane@gmail.com>
2024-05-04 23:51:40 +0530

    update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="about-me-div">
-				<h4 className="about-me-heading">i'm a <span className="student-span">student</span> in my final
-                year of <span className="engineering-span">computer engineering</span>.</h4>
+				<h4 className="about-me-heading">i'm a <span className="software-engineer-span">software engineer</span> at <span className="bloomberg-span">bloomberg</span> working on its
+                 buy-side solution offering called <span className="aim-span">AIM</span>.</h4>
                 <p className="about-me-p">
                     i like writing &#123; <span style={{color: "#43e97b"}}>code</span> &#125; that turn ideas to reality and
-                    i enjoy solving problems with a <span style={{color: "#f6d365"}}>team</span>.
+                    i enjoy solving problems with a <span style={{color: "#f6d365"}}>team</span>. i mostly code in C++, Python, JavaScript/TypeScript at work.
                 </p>
                 <p className="about-me-p">
-                    i dabble in <span style={{color: "#ffb199"}}>machine learning</span> <Emoji symbol="⚙️" label="gear" rotate={true}/> and
+                    i sometimes dabble a little in <span style={{color: "#ffb199"}}>machine learning</span> <Emoji symbol="⚙️" label="gear" rotate={true}/> and
                     <span style={{color: "#92fe9d"}}> front-end web development</span> <Emoji symbol="🕸️" label="web"/>.
                 </p>
                 <p className="about-me-p">
